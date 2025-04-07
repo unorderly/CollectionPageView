3968e29d7666cfb6de22616105ca0c52d9a07074
Leo Mehlig <leo@leomehlig.com>
2025-04-07 21:03:53 +0200

    update tools version

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(name: "CollectionPageView",
                       platforms: [
-                          .iOS(.v16)
+                        .iOS(.v16), .watchOS(.v9), .macCatalyst(.v16), .macOS(.v13)
                       ],
                       products: [
                           .library(name: "CollectionPageView",
